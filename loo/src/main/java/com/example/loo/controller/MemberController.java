c6ff0dff2be6d735050fb1845926646bca710bbe
chang <ckdtns5262@nate.com>
2023-09-20 01:49:28 +0900

    [0920] 로그인,회원가입 페이지 띄우

--- loo/src/main/java/com/example/loo/controller/MemberController.java
+++ loo/src/main/java/com/example/loo/controller/MemberController.java
@@ -1,8 +1,21 @@
 package com.example.loo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class MemberController {
 
+	@GetMapping("/users/signup")
+	public String signUp() {
+		return "users/signup";
+	}
+	
+	@GetMapping("/users/login")
+	public String login() {
+		return "users/login";
+		
+	}
+	
 }
--- loo/src/main/java/com/example/loo/model/member/Member.java
+++ loo/src/main/java/com/example/loo/model/member/Member.java
@@ -1,5 +1,20 @@
 package com.example.loo.model.member;
 
-public class Member {
+import java.time.LocalDateTime;
 
+import lombok.Data;
+
+@Data
+public class Member {
+	private String member_id;
+	private String company_mail;
+	private String password;
+	private String name;
+	private String phone_number;
+	private String birthday;
+	private LocalDateTime hire_date;
+	private String company_id;
+	private String department_id;
+	private String position_id;
+	
 }
