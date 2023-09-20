3df3dbd7b710aa953e16154a8c7742e1d746ec03
m951005 <m951005@gmail.com>
2023-09-20 17:56:03 +0900

    login,signup form kansei

--- loo/src/main/java/com/example/loo/controller/MemberController.java
+++ loo/src/main/java/com/example/loo/controller/MemberController.java
@@ -1,18 +1,32 @@
 package com.example.loo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.loo.model.member.Member;
 
+import lombok.extern.slf4j.Slf4j;
+
+@RequestMapping("users")
 @Controller
+@Slf4j
 public class MemberController {
 
-	@GetMapping("/users/signup")
-	public String signUp() {
+	@GetMapping("signup")
+	public String signUp(Model model) {
+		model.addAttribute("member", new Member());
 		return "users/signup";
 	}
 	
-	@GetMapping("/users/login")
+	@PostMapping("signup")
+	public String signup() {
+		return "redirect:/";
+	}
+	
+	@GetMapping("login")
 	public String login() {
 		return "users/login";
 		
