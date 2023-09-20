5f56fe71848f444757c2233c938c513b82db9205
m951005 <m951005@gmail.com>
2023-09-20 10:00:23 +0900

    ing

--- loo/src/main/java/com/example/loo/controller/MemberController.java
+++ loo/src/main/java/com/example/loo/controller/MemberController.java
@@ -1,8 +1,33 @@
 package com.example.loo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.loo.model.member.Member;
+import com.example.loo.repository.MemberMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
+@RequestMapping("member")
 public class MemberController {
-
+	
+	@Autowired
+	private MemberMapper memberMapper;
+	
+	@GetMapping("join")
+	public String joinForm(Model model) {
+		model.addAttribute("member", new Member());
+		return "member/join";
+	}
+	
+	@PostMapping("join")
+	public String join() {
+		return "redirect:/";
+	}
 }
--- loo/src/main/java/com/example/loo/model/member/Member.java
+++ loo/src/main/java/com/example/loo/model/member/Member.java
@@ -1,5 +1,19 @@
 package com.example.loo.model.member;
 
-public class Member {
+import java.time.LocalDate;
 
+import lombok.Data;
+
+@Data
+public class Member {
+	private Long member_id;
+	private String company_mail;
+	private String password;
+	private String name;
+	private String phone_number;
+	private LocalDate birthday;
+	private LocalDate hire_date;
+	private String company_id;
+	private String department_id;
+	private String position_id;
 }
