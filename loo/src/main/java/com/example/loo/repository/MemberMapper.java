fdbad188c5946cc4c68c6e71f29a8f76a46254da
Cho Hyeonbi <127538099+bivi99@users.noreply.github.com>
2023-09-25 16:30:17 +0900

    Login and Signup finish

--- loo/src/main/java/com/example/loo/repository/MemberMapper.java
+++ loo/src/main/java/com/example/loo/repository/MemberMapper.java
@@ -2,6 +2,10 @@ package com.example.loo.repository;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.loo.model.member.Member;
+
 @Mapper
 public interface MemberMapper {
+	void saveMember(Member member);
+	Member findMember(String company_mail);
 }
