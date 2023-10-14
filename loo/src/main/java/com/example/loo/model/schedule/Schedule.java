b07cb159ce27166c90eb32e1a007098f666880fa
chang <ckdtns5262@nate.com>
2023-10-14 19:16:35 +0900

    [1014] 스케줄 기능 구현중(구글캘린더 연결해서 띄우기안됨)

--- loo/src/main/java/com/example/loo/model/schedule/Schedule.java
+++ loo/src/main/java/com/example/loo/model/schedule/Schedule.java
@@ -1,5 +1,25 @@
 package com.example.loo.model.schedule;
 
-public class Schedule {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class Schedule {
+	
+	private Long schedule_id;
+	private String schedule_name;
+	private String schedule_contents;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate schedule_date;
+	private String member_mail;
+	private String schedule_type;
+	private String importance;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate deadline;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate complete_time;
 }
