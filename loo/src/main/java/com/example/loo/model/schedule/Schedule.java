46b21f2314f8fad68f4aa913d9d4b21ad4ac36e6
chang <ckdtns5262@nate.com>
2023-10-18 19:08:40 +0900

    [1018] merge 재작업2

--- loo/src/main/java/com/example/loo/model/schedule/Schedule.java
+++ loo/src/main/java/com/example/loo/model/schedule/Schedule.java
@@ -13,13 +13,13 @@ public class Schedule {
 	private Long schedule_id;
 	private String schedule_name;
 	private String schedule_contents;
-	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
 	private LocalDateTime schedule_date;
 	private String member_mail;
 	private String schedule_type;
 	private String importance;
-	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
 	private LocalDateTime deadline;
-	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
 	private LocalDateTime complete_time;
 }
