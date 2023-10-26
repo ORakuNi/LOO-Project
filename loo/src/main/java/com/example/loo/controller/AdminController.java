883f057d6552584e29149180af9b7d71c3028c2a
KSJ <gga04002@gmail.com>
2023-10-26 17:14:42 +0900

    SJ Version4.8

--- loo/src/main/java/com/example/loo/controller/AdminController.java
+++ loo/src/main/java/com/example/loo/controller/AdminController.java
@@ -1,14 +1,19 @@
 package com.example.loo.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,7 +53,7 @@ public class AdminController {
 	}
 	
 	@GetMapping("admin-update")
-	public String admin_update(@RequestParam String member_mail,
+	public String adminUpdate(@RequestParam String member_mail,
 								Model model) {
 		
 		Member member = memberMapper.findMember(member_mail);
@@ -59,7 +64,7 @@ public class AdminController {
 	}
 	
 	@PostMapping("admin-update")
-	public String admin_pdate(@RequestParam String member_mail,
+	public String adminUpdate(@RequestParam String member_mail,
 							@Validated @ModelAttribute("update") MemberAdminUpdate memberAdminUpdate,
 							BindingResult result) {
 		
@@ -72,7 +77,7 @@ public class AdminController {
 		
 		Member member = memberMapper.findMember(member_mail);
 		if(member == null) {
-			return "redirect:/";
+			return "redirect:/admin/admin";
 		}
 		
 		member = MemberAdminUpdate.toMember(memberAdminUpdate);
@@ -84,7 +89,7 @@ public class AdminController {
 	}
 	
 	@GetMapping("admin-commute")
-	public String admin_commute(@RequestParam String member_mail, 
+	public String adminCommute(@RequestParam String member_mail, 
 								Model model) {
 		
 		List<Commute> findAllCommutes = commuteMapper.findAllCommutes(member_mail);
@@ -93,5 +98,64 @@ public class AdminController {
 		
 		return "admin/admin-commute";
 	}
+	
+
+	@PostMapping("{commute_id}")
+	public ResponseEntity<String> adminCommuteUpdate(@PathVariable Long commute_id,
+						@RequestParam Map<String, Object> param) {
+		log.info("관리자 일정 변경");
+		log.info("commute_id: {}", commute_id);
+		log.info("param: {}", param);
+		
+		Commute commute = commuteMapper.findCommute(commute_id);
+		
+//		if(commute == null) {
+//			
+//		}
+		
+		// Map 타입으로 가져온 param들을 String 으로 각각 담아줌
+		String attendance = (String)param.get("attendance_time");
+		String leave = (String)param.get("leave_time");
+		
+		// 다시 String 타입으로 바꾼 param들을 LocalDateTime 으로 형변환 
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+		LocalDateTime attendance_time = LocalDateTime.parse(attendance, formatter);
+		LocalDateTime leave_time = LocalDateTime.parse(leave, formatter);
+		
+		
+		commute.setAttendance_time(attendance_time);
+		commute.setLeave_time(leave_time);
+		
+		commuteMapper.updateAdminCommute(commute);
+		
+		return ResponseEntity.ok("업데이트 성공");
+	}
+	
+	
+	
+	
+//	@GetMapping("admin-commute-update")
+//	public String adminCommuteUpdate(@RequestParam Long commute_id,
+//						@Validated @ModelAttribute("commutes") CommuteAdminUpdate commuteAdminUpdate) {
+//		log.info("관리자 일정 변경");
+//		log.info("commute_id: {}", commute_id);
+//		
+//		Commute commute = commuteMapper.findCommute(commute_id);
+//		log.info("commute: {}", commute);
+//		if(commute == null) {
+//			return "redirect:/admin/admin";
+//		}
+//		
+//		commute.setAttendance_time(commuteAdminUpdate.getAttendance_time());
+//		commute.setLeave_time(commuteAdminUpdate.getLeave_time());
+//		
+//		log.info("commuteAdminUpdate: {}", commuteAdminUpdate);
+//		log.info("commute: {}", commute);
+//		commuteMapper.updateAdminCommute(commute);
+//		
+//		
+//		
+//		return "redirect:/admin/admin";
+//	}
 	
 }
--- loo/src/main/java/com/example/loo/repository/CommuteMapper.java
+++ loo/src/main/java/com/example/loo/repository/CommuteMapper.java
@@ -13,13 +13,12 @@ public interface CommuteMapper {
 
 	// 출근하기
 	void insertCommute(Commute commute);
-	
 	// 퇴근하기 
 	void updateCommute(Commute commute);
-	
 	// 찾기
 	Commute findCommute(Long commute_id);
-	
+	// 관리자용 출퇴근 기록 수정
+	void updateAdminCommute(Commute commute);
 	// 전체 출퇴근 기록
 	List<Commute> findAllCommutes(String member_mail); 
 }
